a57687b960d78bbef234a29dc8c072c60e434f41
tharaka911 <tharakahitibandara@gmail.com>
2023-10-24 23:40:01 +0530

    setp-3

--- users-adding.sh
+++ users-adding.sh
@@ -2,43 +2,41 @@
 
 # Define an array with user information
 users=(
-    "lakshan_sl \"<add-key>\""
+    "lakshan_sl \"ssh_key1\" \"password1\""
     # Add more users as needed
 )
 
-# Iterate through the array and create users with SSH keys
+# Iterate through the array and create users with SSH keys and passwords
 for user_info in "${users[@]}"; do
-    # Split the user information into username and public key
-    read -r username public_key <<< "$user_info"
+    # Split the user information into username, public key, and password
+    read -r username public_key password <<< "$user_info"
 
-    # Remove the double quotes around the public key
+    # Remove the double quotes around the public key and password
     public_key="${public_key//\"/}"
+    password="${password//\"/}"
 
-    # Create a new user
-    sudo adduser "$username"
+    # Create a new user with the specified password
+    sudo useradd -m -p "$(openssl passwd -1 "$password")" "$username"
 
     # Add the user to the sudo group
     sudo usermod -aG sudo "$username"
 
     # Switch to the user's home directory
     user_home_directory="/home/$username"
-    cd "$user_home_directory"
 
-    # Create the .ssh directory
-    mkdir -p .ssh
-
-    # Change to the .ssh directory
-    cd .ssh
+    # Create the .ssh directory and authorized_keys file
+    sudo -u $username mkdir -p "$user_home_directory/.ssh"
+    sudo -u $username touch "$user_home_directory/.ssh/authorized_keys"
 
     # Add the public key to authorized_keys
-    echo "$public_key" >> authorized_keys
+    echo "$public_key" | sudo -u $username tee -a "$user_home_directory/.ssh/authorized_keys" > /dev/null
 
     # Set proper permissions
-    chmod 700 "$user_home_directory"
-    chmod 700 .ssh
-    chmod 600 authorized_keys
+    sudo -u $username chmod 700 "$user_home_directory"
+    sudo -u $username chmod 700 "$user_home_directory/.ssh"
+    sudo -u $username chmod 600 "$user_home_directory/.ssh/authorized_keys"
 
-    echo "User $username created with SSH key."
+    echo "User $username created with SSH key and password."
 
     # Example: How to log in using the SSH key
     echo "To log in as $username, use the following command:"
